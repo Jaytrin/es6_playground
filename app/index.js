7839417d23c026ad841bc47f4282921abd3aa5ce
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-09-10 16:09:51 -0700

    import and export functions

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import addToDom from './add_to_dom';
+import {subtract} from './math';
+import * as math from './math'
+// import str, {subtract} from './math'; 
+//This is what the default is
+
+console.log('Math:', math);
+
+addToDom('h3', `52 - 10 = ${subtract(52,10)}`);
